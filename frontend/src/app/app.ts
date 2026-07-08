0fcb1ce3dc25c628f3778637387963994abf31cc
Antonio <antoniomartino0067@gmail.com>
2026-07-08 18:09:00 +0200

    Aggiunto vercel analytics

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from "@vercel/analytics"
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor() {
+    inject();
+  }
 }
